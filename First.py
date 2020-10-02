294ea3fa6ca907b4a3254b9be6545f0772ba9cbd
ishika15 <72149415+ishika15@users.noreply.github.com>
2020-10-02 11:10:12 +0530

    Update First.py

--- First.py
+++ First.py
@@ -24,3 +24,15 @@ for i in range(len(X)):
 
 for r in result:
    print(r)
+
+#program to multiply factorial of a number
+num = int(input("Enter a number: "))  
+factorial = 1  
+if num < 0:  
+   print("Sorry, factorial does not exist for negative numbers")  
+elif num == 0:  
+   print("The factorial of 0 is 1")  
+else:  
+   for i in range(1,num + 1):  
+       factorial = factorial*i  
+   print("The factorial of",num,"is",factorial)  
